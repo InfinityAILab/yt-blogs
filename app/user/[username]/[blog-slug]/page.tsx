cd97220dd6546c7d9ecaaa8c0e5a606fb2aae9f2
Danish <danishasifdev@gmail.com>
2024-06-27 14:57:15 +0500

    feat: made blog page responsive

--- app/user/[username]/[blog-slug]/page.tsx
+++ app/user/[username]/[blog-slug]/page.tsx
@@ -25,9 +25,9 @@ const BlogPage = () => {
 
   return (
     <div className="bg-gray-100">
-      <div className="max-w-[1550px]  p-4 rounded-lg mx-auto pt-10 grid grid-cols-10 gap-2">
-        <aside className="sticky top-20 h-[calc(100vh-4rem)]  p-4 rounded-lg col-span-1 mt-20">
-          <div className="flex flex-col items-end justify-center gap-12 mr-3">
+      <div className="max-w-[1550px] sm:p-4 rounded-lg mx-auto pt-10 md:grid sm:grid-cols-6 lg:grid-cols-10 gap-2">
+        <aside className="md:sticky top-20 h-[calc(100vh-4rem)]  p-4 rounded-lg sm:col-span-1 mt-20 hidden md:block">
+          <div className="flex flex-col sm:items-center md:items-end justify-center gap-12 mr-3">
             <div className="flex items-center justify-center hover:cursor-pointer flex-col gap-1.5 hover:text-red-500">
               <HeartIcon className="w-8 h-8 " />
               <span className="text-xs ">100+</span>
@@ -45,16 +45,16 @@ const BlogPage = () => {
             </div>
           </div>
         </aside>
-        <main className="py-4 px-12 bg-white col-span-7 rounded-lg ">
-          <h2 className="text-start text-4xl mt-4 mb-8 font-semibold">
+        <main className="py-4 px-2 md:px-12 bg-white sm:col-span-5 lg:col-span-7 rounded-lg max-w-5xl">
+          <h2 className="text-start text-3xl sm:text-4xl my-4 sm:mt-4 sm:mb-8 font-semibold">
             My Blog Post
           </h2>
           <div>
-            <div className="flex gap-4 items-center">
-              <div className="bg-gray-600 w-10 h-10 rounded-full"></div>
+            <div className="flex gap-2 sm:gap-4 items-center">
+              <div className="bg-gray-600 w-8 h-8 sm:w-10 sm:h-10 rounded-full"></div>
               <div>
                 <h4 className="font-bold mb-0.5">Chris Bongers</h4>
-                <p className="font-light text-sm">
+                <p className="font-light text-xs sm:text-sm">
                   Posted on 20 Jan 2022 • Originally published at
                   daily-dev-tips.com
                 </p>
@@ -66,7 +66,7 @@ const BlogPage = () => {
             dangerouslySetInnerHTML={{ __html: mdParser.render(text) }}
           />
         </main>
-        <aside className="h-[calc(100vh-20rem)] p-4 rounded-lg col-span-2 bg-white">
+        <aside className="h-[calc(100vh-20rem)] p-4 rounded-lg col-span-2 bg-white hidden lg:block">
           <div className="flex flex-col gap-4">
             <div className="flex gap-4 items-end">
               <div className="bg-gray-600 w-10 h-10 rounded-full"></div>
