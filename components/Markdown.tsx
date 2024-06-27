582bb973fc3776e7f5fccb2a9450176a2974aea2
Danish <135382273+danishasifdev@users.noreply.github.com>
2024-06-27 16:36:26 +0500

    Feat/add new markdown (#4)
    * feat: added markdown editor lite
    
    * feat: added blog page with routing
    
    * feat: added new dynamic user blog page
    
    * feat : add profile page part 1
    
    * feat:completed profile page and made responsive as well
    
    * feat: made blog page responsive
    
    * feat: added google analytics

---
+++ components/Markdown.tsx
@@ -0,0 +1,46 @@
+"use client"
+// components/MarkdownEditor.tsx
+import dynamic from "next/dynamic"
+import { useState } from "react"
+import markdownIt from "markdown-it"
+import "react-markdown-editor-lite/lib/index.css"
+import "./../app/globals.css"
+
+// Define the types we need
+interface EditorProps {
+  value: string
+  style?: React.CSSProperties
+  onChange: (data: { text: string; html: string }) => void
+  renderHTML: (text: string) => React.ReactNode
+  className?: string
+}
+
+// Use dynamic import for the editor
+const MdEditor = dynamic(() => import("react-markdown-editor-lite"), {
+  ssr: false,
+}) as React.ComponentType<EditorProps>
+
+const mdParser = new markdownIt()
+
+export default function MarkdownEditor({ text, setText }: any) {
+  const handleEditorChange = ({ text }: { text: string }) => {
+    setText(text)
+  }
+
+  return (
+    <div className="overflow-auto">
+      <MdEditor
+        style={{ height: "650px" }}
+        value={text}
+        onChange={(text) => handleEditorChange(text)}
+        renderHTML={(text) => (
+          <div
+            className="markdown-body prose"
+            dangerouslySetInnerHTML={{ __html: mdParser.render(text) }}
+          />
+        )}
+        className="border border-gray-300 rounded-lg"
+      />
+    </div>
+  )
+}
