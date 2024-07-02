9abf5c5e284be9539ff7e4600330cc0ed6062a11
Saad Gulzar <isaadgulzar@gmail.com>
2024-07-02 07:24:22 +0500

    feat: add toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import Script from "next/script"
+import { Toaster } from "@/components/ui/sonner"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -32,7 +33,10 @@ export default function RootLayout({
           `}
         </Script>
       </head>
-      <body className={`h-full w-full overflow-hidden relative ${inter.className}`}>{children}</body>
+      <body className={`h-full w-full relative ${inter.className}`}>
+        {children}
+        <Toaster richColors />
+      </body>
     </html>
   )
 }
